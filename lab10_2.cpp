d05b3c62ba48cb40b5cad6d9b56357825ad97625
Sipurit <gun0820303553@gmail.com>
2024-12-17 15:49:33 +0700

    #

--- lab10_2.cpp
+++ lab10_2.cpp
@@ -6,12 +6,12 @@ using namespace std;
 int main (){
 	ifstream source;
 	ofstream dest;
-	source.open("cheerbook.txt") ;
+	source.open("cheerbook.txt");
 	dest.open("cheerbook_copy.txt");
 	string cheerbook;
 	dest << "-------------------- BOOM ---------------------\n";
 	while(getline(source,cheerbook)){
-		dest << cheerbook << "\n";
+		dest << cheerbook << endl;
 	}
 	dest << "-------------------- HA!! ---------------------";
     source.close();
